1b4f94219a252d6c39283ab1926e1051557a56c9
subbu76 <30485999+subbu76@users.noreply.github.com>
2022-01-07 16:39:24 -0500

    Add files via upload

---
+++ Install-IIS.ps1
@@ -0,0 +1,8 @@
+Install-Windows Feature Web-Server
+
+$htmlContent="<html><body><h1>Hell you are logged into " + $($env:computername) +
+            "</h1></body></html";
+
+$htmlFile = "C:\inetpub\wwwroot\Default.htm"
+
+Add-Content -Value $htmlContent -Path $htmlFile
